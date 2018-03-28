76fab231084599c93776ca76089cc014f2004748
LUMIA-N9 <1044740758@qq.com>
2018-03-28 17:31:09 +0800

    add new .gitignore & remove the old .gitignore & add new file 'models.py' & make some changes in orm.py

--- www/orm.py
+++ www/orm.py
@@ -17,7 +17,7 @@ async def create_pool(loop, **kw):
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
-        charset=kw.get('charset', 'utf-8'),
+        charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit',
                           True),  # 默认自动提交事物，不用手动去提交事务，即不用cur.commit()
         maxsize=kw.get('maxsize', 10),
@@ -79,6 +79,21 @@ class IntegerField(Field):
         super().__init__(name, ddl, primary_key, default=0)
 
 
+class BooleanField(Field):
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+
 def create_args_string(num):
     L = []
     for n in range(num):
@@ -93,7 +108,8 @@ class ModelMetaclass(type):
             return type.__new__(cls, name, bases, attrs)
         # 获取table的名称
         tableName = attrs.get('__table__', None) or name
-        logging.info('found model: %s (table: %s') % (name, tableName)
+        print(attrs)
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         # 获取所有的Field和主键名：
         mappings = dict()
         fields = []
@@ -110,10 +126,12 @@ class ModelMetaclass(type):
                     primaryKey = k  # k只是一个str，用来保存类属性or类方法的名字，没有实际意思，v就有实际意义了
                 else:
                     fields.append(k)
+        print(fields)
         if not primaryKey:
             raise RuntimeError('Primary key not found.')
         for k in mappings.keys():  # 这里的keys是一个dict方法。。。。
             attrs.pop(k)  # 这里为什么要pop，可参照水友评论，很强,不弹出来会导致实例化User的时候，参数值传不进去
+            # 注意这里pop的是attrs，而不是mappings，mappings仍然保存着所有Field类型的 类 的属性
         escaped_fields = list(map(lambda f: '`%s`' % f,
                                   fields))  # 把除主键以外的属性都换成``反引号
         attrs['__mappings__'] = mappings
@@ -127,7 +145,7 @@ class ModelMetaclass(type):
             create_args_string(len(escaped_fields) + 1))
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (
             tableName, ', '.join(
-                map(lambda f: '%s`=?' % (mappings.get(f).name or f), fields)),
+                map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)),
             primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName,
                                                                  primaryKey)
@@ -158,8 +176,8 @@ class Model(dict, metaclass=ModelMetaclass):
                 value = field.default() if callable(
                     field.default
                 ) else field.default  # 如果field.default是可调用对象，那么field.default()就可以调用，就有返回值;反之则调用field.default获取值
-                logging.debug('suding default value for %s: %s' % (key,
-                                                                   str(value)))
+                logging.debug('using default value for %s: %s' % (key,
+                                                                  str(value)))
                 setattr(self, key, value)
         return value
 
@@ -232,10 +250,3 @@ class Model(dict, metaclass=ModelMetaclass):
         if rows != 1:
             logging.warn(
                 'failed to remove by primary key: affected rows: %s' % rows)
-
-
-class User(Model):
-    __table__ = 'users'
-
-    id = IntegerField(primary_key=True)
-    name = StringField()
